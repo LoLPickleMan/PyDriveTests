9104a4b2d7ddbd263589638007f0f47b52ad0bf2
PrilepaAndrii <andrii.prylepa@modelingevolution.com>
2022-08-23 08:45:32 +0200

    change

--- pwmTest.py
+++ pwmTest.py
@@ -5,12 +5,12 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(7, GPIO.OUT)
 
-t1 = GPIO.PWM(7, 8000)
+t1 = GPIO.PWM(7, 50)
 
 t1.start(0)
-
+#some changed
 t1.ChangeDutyCycle(7.5)
-time.sleep(300)
+time.sleep(30)
 
 t1.stop()
 
